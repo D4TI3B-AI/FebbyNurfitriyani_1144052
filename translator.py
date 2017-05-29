910292f8edae84327710d31767d57d877c61d59b
febbynurfitriyani1208@gmail.com <febbynurfitriyani1208@gmail.com>
2017-05-29 13:43:00 +0700

    add pertemuan 5

---
+++ translator.py
@@ -0,0 +1,10 @@
+import goslate
+gs = goslate.Goslate()
+print('Pilih Bahasa :')
+print('1.Indonesia Ke Inggris')
+print('2.Inggris Ke Indonesia')
+a = input("Pilihan : ")
+if a == '1'    : kata = input("Masukan : ")
+print(gs.translate(kata, 'id'))
+if a == '2'    : kata = input("Enter : ")
+print(gs.translate(kata, 'en'))
